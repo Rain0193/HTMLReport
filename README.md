321c531b470bdd03ad8293c00009658c004bcdb7
刘士 <liushilive@outlook.com>
2017-07-26 11:17:20 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTMLReport
+
+`HTMLReport`是一个单元测试测试运行器，可以将测试结果保存在Html文件中，用于人性化的结果显示。<br><br>仅支持**Python 3.x**
+
+
+# 安装
+要安装HTMLReport，请在终端中运行此命令
+```py
+$ pip install HTMLReport
+```
+这是安装HTMLReport的首选方法，因为它将始终安装最新的稳定版本。如果您没有安装[pip](https://pip.pypa.io/)，则该[Python安装指南](http://docs.python-guide.org/en/latest/starting/installation/ "Python安装指南")可以指导您完成该过程。
+
+# 用法
+```py
+
+```
+
+# 控制台输出：
+
+
+# 测试结果：
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='HTMLReport',
+    version='0.0.1',
+    description="Python3 THML报告生成",
+    long_description=__doc__,
+    author="刘士",
+    author_email='liushilive@outlook.com',
+    url='',
+    packages=[
+        'HTMLReport',
+    ],
+    package_dir={'HTMLReport':
+                 'HTMLReport'},
+    include_package_data=True,
+    license="MIT license",
+    zip_safe=False,
+    keywords='Html Reports',
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests'
+)
