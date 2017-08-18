0b8ff1ef68408e189a7363a75cec1753aa7c930d
刘士 <liushilive@outlook.com>
2017-08-18 10:04:43 +0800

    添加注释

--- HTMLReport/HTMLReport.py
+++ HTMLReport/HTMLReport.py
@@ -14,7 +14,7 @@ from HTMLReport.Redirector import OutputRedirector
 from HTMLReport.Template import TemplateMixin
 
 __author__ = "刘士"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # 日志输出
 #   >>> logging.basicConfig(stream=HTMLReport.stdout_redirector)
@@ -120,6 +120,24 @@ class TestRunner(TemplateMixin):
     def __init__(self, report_file_name: str = 'test', output_path: str = None, title: str = None,
                  description: str = None, verbosity: int = 2, thread_count: int = 1,
                  sequential_execution: bool = False):
+        """
+        runner = HTMLReport.TestRunner(report_file_name='test',  # 报告文件名，默认“test”
+                                   output_path='report',  # 保存文件夹名，默认“report”
+                                   verbosity=2,  # 控制台输出详细程度，默认 2
+                                   title='测试报告',  # 报告标题，默认“测试报告”
+                                   description='无测试描述',  # 报告描述，默认“无测试描述”
+                                   thread_count=2,  # 是否多线程测试（无序执行），默认 1
+                                   sequential_execution=True  # 是否按照套件添加(addTests)顺序执行，
+                                   # 会等待一个addTests执行完成，再执行下一个，默认 False
+                                   )
+        :param report_file_name: 报告文件名
+        :param output_path:保存文件夹名
+        :param title:报告标题
+        :param description:报告描述
+        :param verbosity:控制台输出详细程度
+        :param thread_count:是否多线程测试（无序执行）
+        :param sequential_execution:是否按照套件添加(addTests)顺序执行， 会等待一个addTests执行完成，再执行下一个，默认 False
+        """
         self.output_path = output_path or "report"
         self.title = title or self.DEFAULT_TITLE
         self.description = description or self.DEFAULT_DESCRIPTION
