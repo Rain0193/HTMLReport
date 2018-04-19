97a4e4d4ab0a3cc76c93b6f28ffb1a3440246ef0
刘士 <liushilive@outlook.com>
2018-04-19 17:59:16 +0800

    １.３.９

--- HTMLReport/Template.py
+++ HTMLReport/Template.py
@@ -13,8 +13,7 @@ class TemplateMixin(object):
     DEFAULT_DESCRIPTION = '测试描述'
     DEFAULT_DESCRIPTION_en = 'Test Description'
 
-    HTML_TMPL = r"""
-<?xml version="1.0" encoding="UTF-8"?>
+    HTML_TMPL = r"""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -98,11 +97,11 @@ function showClassDetail(cid, count) {
     if (!tr) {
       tid = "p" + tid0;
       tr = document.getElementById(tid);
-      if (tr === null) {
-        tid = "s" + tid0;
+      if (!tr) {
+        tid = "e" + tid0;
         tr = document.getElementById(tid);
-        if (!tr) {
-          tid = "e" + tid0;
+        if (tr === null) {
+          tid = "s" + tid0;
           tr = document.getElementById(tid);
         }
       }
--- HTMLReport/__init__.py
+++ HTMLReport/__init__.py
@@ -1,5 +1,5 @@
 __author__ = '刘士'
-__version__ = '1.3.8'
+__version__ = '1.3.9'
 
 from .HTMLReport import TestRunner
 from .images.SaveImages import AddImage
